91c0e7b01cce7d3a008d14f911d48a3d3a4273e0
Tom <tomdion35@gmail.com>
2025-01-22 12:10:54 -0500

    cleanup backend

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
+from .serializers import CustomUserSerializer, RegisterUserSerializer, LoginUserSerializer
 from rest_framework.generics import RetrieveUpdateAPIView, CreateAPIView
 from rest_framework.permissions import IsAuthenticated
-from .serializers import CustomUserSerializer, RegisterUserSerializer, LoginUserSerializer
 from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework_simplejwt.exceptions import InvalidToken
 # Create your views here.
