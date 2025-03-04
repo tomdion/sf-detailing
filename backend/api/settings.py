386cf79975d0a2b4081284f759a2ab2cd991f126
Tom <tomdion35@gmail.com>
2025-03-04 02:18:16 +0000

    added editable packages

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -131,6 +131,8 @@ AUTH_USER_MODEL = 'users.CustomUser'
 REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
+
+        'rest_framework.authentication.SessionAuthentication',
         
         'users.authentication.CookieJWTAuthentication',
     )
