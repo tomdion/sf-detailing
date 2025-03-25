d4cdb08a0b7b9f95dedc1cead54b2ce8bfb1c578
Tom <tomdion35@gmail.com>
2025-03-25 03:51:07 +0000

    fixes export default function

---
+++ frontend/src/app/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/ui/Navbar";
+
+export default function SignUp() {
+    return (
+      <main className="min-h-screen">
+        <Navbar />
+      </main>
+    );
+  }
